643119b6a53ffdf5689fd3a571362beb9cfd9c42
tnyo43 <tnyo43@gmail.com>
2022-10-15 17:19:17 +0900

    implement all top corner rotate

--- src/CubeView.elm
+++ src/CubeView.elm
@@ -1119,7 +1119,45 @@ displayedArrowsOfSelectedObject q selected =
                     []
 
         Corner n ->
-            []
+            case n of
+                0 ->
+                    [ { arrow = Vector.vector 2 1 0, rotateInfo = { rotateTarget = Side Top, direction = CCW } }
+                    , { arrow = Vector.vector 1 2 0, rotateInfo = { rotateTarget = Side Top, direction = CW } }
+                    , { arrow = Vector.vector 1 0 -2, rotateInfo = { rotateTarget = Side Left, direction = CCW } }
+                    , { arrow = Vector.vector 2 0 -1, rotateInfo = { rotateTarget = Side Left, direction = CW } }
+                    , { arrow = Vector.vector 0 2 -1, rotateInfo = { rotateTarget = Side Back, direction = CCW } }
+                    , { arrow = Vector.vector 0 1 -2, rotateInfo = { rotateTarget = Side Back, direction = CW } }
+                    ]
+
+                1 ->
+                    [ { arrow = Vector.vector 1 -2 0, rotateInfo = { rotateTarget = Side Top, direction = CCW } }
+                    , { arrow = Vector.vector 2 -1 0, rotateInfo = { rotateTarget = Side Top, direction = CW } }
+                    , { arrow = Vector.vector 2 0 -1, rotateInfo = { rotateTarget = Side Right, direction = CCW } }
+                    , { arrow = Vector.vector 1 0 -2, rotateInfo = { rotateTarget = Side Right, direction = CW } }
+                    , { arrow = Vector.vector 0 -1 -2, rotateInfo = { rotateTarget = Side Back, direction = CCW } }
+                    , { arrow = Vector.vector 0 -2 -1, rotateInfo = { rotateTarget = Side Back, direction = CW } }
+                    ]
+
+                2 ->
+                    [ { arrow = Vector.vector -2 -1 0, rotateInfo = { rotateTarget = Side Top, direction = CCW } }
+                    , { arrow = Vector.vector -1 -2 0, rotateInfo = { rotateTarget = Side Top, direction = CW } }
+                    , { arrow = Vector.vector -1 0 -2, rotateInfo = { rotateTarget = Side Right, direction = CCW } }
+                    , { arrow = Vector.vector -2 0 -1, rotateInfo = { rotateTarget = Side Right, direction = CW } }
+                    , { arrow = Vector.vector 0 -2 -1, rotateInfo = { rotateTarget = Side Front, direction = CCW } }
+                    , { arrow = Vector.vector 0 -1 -2, rotateInfo = { rotateTarget = Side Front, direction = CW } }
+                    ]
+
+                3 ->
+                    [ { arrow = Vector.vector -1 2 0, rotateInfo = { rotateTarget = Side Top, direction = CCW } }
+                    , { arrow = Vector.vector -2 1 0, rotateInfo = { rotateTarget = Side Top, direction = CW } }
+                    , { arrow = Vector.vector -2 0 -1, rotateInfo = { rotateTarget = Side Left, direction = CCW } }
+                    , { arrow = Vector.vector -1 0 -2, rotateInfo = { rotateTarget = Side Left, direction = CW } }
+                    , { arrow = Vector.vector 0 1 -2, rotateInfo = { rotateTarget = Side Front, direction = CCW } }
+                    , { arrow = Vector.vector 0 2 -1, rotateInfo = { rotateTarget = Side Front, direction = CW } }
+                    ]
+
+                _ ->
+                    []
     )
         |> List.map
             (\arrowInfo ->
